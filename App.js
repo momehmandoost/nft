6b18f7efa0ea644b3ca02b7225fde3ca77274aea
Mohammad <mehmandoost2@gmail.com>
2023-08-23 20:51:43 +0430

    Add navigation

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
 import Slider from './src/component/Slider';
 import { useFonts } from "expo-font";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Slider />
+    </SafeAreaView>
+  );
+}
 
 const App = () => {
   const [fontsLoaded] = useFonts({
     'Inter': require('./assets/fonts/Inter.ttf'),
     'MuseoModerno': require('./assets/fonts/MuseoModerno-Bold.ttf'),
-
-
   });
 
   if (!fontsLoaded) {
     return null;
   }
+
   return (
-    <SafeAreaView>
-      <Slider />
-    </SafeAreaView>
+    <NavigationContainer>
+      <StatusBar backgroundColor="purple" barStyle="light-content" />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default App;
